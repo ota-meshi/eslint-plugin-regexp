cd53e9d1b242c4f9c1a748596aff556db022c3eb
yosuke ota <otameshiyo23@gmail.com>
2021-03-03 22:37:04 +0900

    Update

--- lib/utils/type-tracker/jsdoc/index.ts
+++ lib/utils/type-tracker/jsdoc/index.ts
@@ -90,7 +90,7 @@ export class JSDoc {
         }
         const params = (this.params = new JSDocParams())
         for (const param of this.genTags("param")) {
-            const paths: string[] = (param.name || "").split(/\./gu) // todo check for brackets.
+            const paths: string[] = (param.name || "").split(/\./gu)
             params.add(paths, param)
         }
         return params
