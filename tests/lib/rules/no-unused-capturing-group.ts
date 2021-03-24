9b5c8f465cb9d0e18b8504c51c0f561e072e9aed
yosuke ota <otameshiyo23@gmail.com>
2021-03-24 21:57:41 +0900

    Fix false negatives for `split` in `regexp/no-unused-capturing-group` rule

--- tests/lib/rules/no-unused-capturing-group.ts
+++ tests/lib/rules/no-unused-capturing-group.ts
@@ -80,6 +80,14 @@ tester.run("no-unused-capturing-group", rule as any, {
         '2000-12-31 2000-12-31'.replace(reg, '$<y>/$<m>');
         '2000-12-31 2000-12-31'.replace(reg, '$<m>/$<d>');
         `,
+        String.raw`
+        const reg = /,/;
+        'a,b,c'.split(reg)
+        `,
+        String.raw`
+        const reg = /(,)/;
+        'a,b,c'.split(reg)
+        `,
     ],
     invalid: [
         {
@@ -423,9 +431,17 @@ tester.run("no-unused-capturing-group", rule as any, {
                 var m = groups.m // "12"
                 // var d = matches[3] // "31"
             }
-            
             `,
             errors: ["Capturing group is defined but never used."],
+        },
+        {
+            code: String.raw`
+            const reg = /(?<comma>,)/;
+            'a,b,c'.split(reg)
+            `,
+            errors: [
+                "'comma' is defined for capturing group, but it name is never used.",
+            ],
         },
     ],
 })
