59ded29656d4a832190f4530a58367de6f4309f9
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-04-14 17:58:17 +0200

    Added `regexp/no-trivially-nested-assertion` rule

---
+++ lib/rules/no-trivially-nested-assertion.ts
@@ -0,0 +1,139 @@
+import type { Expression } from "estree"
+import type { RegExpVisitor } from "regexpp/visitor"
+import type {
+    Node as RegExpNode,
+    Assertion,
+    LookaroundAssertion,
+} from "regexpp/ast"
+import {
+    createRule,
+    defineRegexpVisitor,
+    fixReplaceNode,
+    getRegexpLocation,
+} from "../utils"
+import { hasSomeDescendant } from "regexp-ast-analysis"
+
+/**
+ * Returns whether the given node is a lookaround.
+ */
+function isLookaround(node: RegExpNode): node is LookaroundAssertion {
+    return (
+        node.type === "Assertion" &&
+        (node.kind === "lookahead" || node.kind === "lookbehind")
+    )
+}
+
+/**
+ * If the given lookaround only contains a single assertion, then this
+ * assertion will be returned.
+ */
+function getTriviallyNestedAssertion(
+    node: LookaroundAssertion,
+): Assertion | null {
+    const alternatives = node.alternatives
+    if (alternatives.length === 1) {
+        const elements = alternatives[0].elements
+        if (elements.length === 1) {
+            const element = elements[0]
+            if (element.type === "Assertion") {
+                return element
+            }
+        }
+    }
+
+    return null
+}
+
+/**
+ * Returns the raw of an assertion that is the negation of the given assertion.
+ */
+function getNegatedRaw(assertion: Assertion): string | null {
+    if (assertion.kind === "word") {
+        return assertion.negate ? "\\b" : "\\B"
+    } else if (assertion.kind === "lookahead") {
+        return `(?${assertion.negate ? "=" : "!"}${assertion.raw.slice(3)}`
+    } else if (assertion.kind === "lookbehind") {
+        return `(?<${assertion.negate ? "=" : "!"}${assertion.raw.slice(4)}`
+    }
+    return null
+}
+
+export default createRule("no-trivially-nested-assertion", {
+    meta: {
+        docs: {
+            description: "disallow trivially nested assertions",
+            recommended: true,
+        },
+        fixable: "code",
+        schema: [],
+        messages: {
+            unexpected: "Unexpected trivially nested assertion.",
+        },
+        type: "suggestion", // "problem",
+    },
+    create(context) {
+        const sourceCode = context.getSourceCode()
+
+        /**
+         * Create visitor
+         * @param node
+         */
+        function createVisitor(node: Expression): RegExpVisitor.Handlers {
+            return {
+                onAssertionEnter(aNode) {
+                    if (aNode.parent.type === "Quantifier") {
+                        // Not all quantifiers are allowed to be the element
+                        // of a quantifier, so we don't even try
+                        return
+                    }
+
+                    if (!isLookaround(aNode)) {
+                        // there can be no nesting in predefined assertions
+                        return
+                    }
+
+                    const nested = getTriviallyNestedAssertion(aNode)
+                    if (nested === null) {
+                        return
+                    }
+
+                    if (
+                        aNode.negate &&
+                        isLookaround(nested) &&
+                        nested.negate &&
+                        hasSomeDescendant(
+                            nested,
+                            (d) => d.type === "CapturingGroup",
+                        )
+                    ) {
+                        // e.g. /(?!(?!(a)))\1/ != /(?=(a))\1/
+                        return
+                    }
+
+                    const replacement = aNode.negate
+                        ? getNegatedRaw(nested)
+                        : nested.raw
+                    if (replacement === null) {
+                        return
+                    }
+
+                    context.report({
+                        node,
+                        loc: getRegexpLocation(sourceCode, node, aNode),
+                        messageId: "unexpected",
+                        fix: fixReplaceNode(
+                            sourceCode,
+                            node,
+                            aNode,
+                            replacement,
+                        ),
+                    })
+                },
+            }
+        }
+
+        return defineRegexpVisitor(context, {
+            createVisitor,
+        })
+    },
+})
---
+++ tests/lib/rules/no-trivially-nested-assertion.ts
@@ -0,0 +1,151 @@
+import { RuleTester } from "eslint"
+import rule from "../../../lib/rules/no-trivially-nested-assertion"
+
+const tester = new RuleTester({
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+    },
+})
+
+tester.run("no-trivially-nested-assertion", rule as any, {
+    valid: [
+        `/(?=(?=a)b)/`,
+
+        // these anchors cannot be negated, so they have to be allowed
+        `/(?!$)/`,
+        `/(?<!$)/`,
+        `/(?!^)/`,
+        `/(?<!^)/`,
+
+        // the text of capturing groups inside negated lookarounds is
+        // guaranteed to be reset, so we can't transform them into one
+        // non-negated lookaround
+        `/(?!(?!(a)))/`,
+    ],
+    invalid: [
+        {
+            code: String(/(?=$)/),
+            output: String(/$/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?=^)/),
+            output: String(/^/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?<=$)/),
+            output: String(/$/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?<=^)/),
+            output: String(/^/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?=\b)/),
+            output: String(/\b/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?!\b)/),
+            output: String(/\B/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?<=\b)/),
+            output: String(/\b/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?<!\b)/),
+            output: String(/\B/),
+            errors: [{ messageId: "unexpected" }],
+        },
+
+        // all trivially nested lookarounds can be written as one lookaround
+        // Note: The inner lookaround has to be negated if the outer one is negative.
+        {
+            code: String(/(?=(?=a))/),
+            output: String(/(?=a)/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?=(?!a))/),
+            output: String(/(?!a)/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?=(?<=a))/),
+            output: String(/(?<=a)/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?=(?<!a))/),
+            output: String(/(?<!a)/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?!(?=a))/),
+            output: String(/(?!a)/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?!(?!a))/),
+            output: String(/(?=a)/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?!(?<=a))/),
+            output: String(/(?<!a)/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?!(?<!a))/),
+            output: String(/(?<=a)/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?<=(?=a))/),
+            output: String(/(?=a)/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?<=(?!a))/),
+            output: String(/(?!a)/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?<=(?<=a))/),
+            output: String(/(?<=a)/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?<=(?<!a))/),
+            output: String(/(?<!a)/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?<!(?=a))/),
+            output: String(/(?!a)/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?<!(?!a))/),
+            output: String(/(?=a)/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?<!(?<=a))/),
+            output: String(/(?<!a)/),
+            errors: [{ messageId: "unexpected" }],
+        },
+        {
+            code: String(/(?<!(?<!a))/),
+            output: String(/(?<=a)/),
+            errors: [{ messageId: "unexpected" }],
+        },
+    ],
+})
