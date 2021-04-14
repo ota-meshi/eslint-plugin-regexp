f28ef5a5eea6fbf20e4fc4cb048d52de3817affb
Michael Schmidt <mitchi5000.ms@googlemail.com>
2021-04-15 01:31:26 +0200

    Removed Windows line ends from tooling (#123)

--- tools/update-rules.ts
+++ tools/update-rules.ts
@@ -1,9 +1,7 @@
 import path from "path"
 import fs from "fs"
-import os from "os"
 // import eslint from "eslint"
 import { rules } from "./lib/load-rules"
-const isWin = os.platform().startsWith("win")
 
 /**
  * Convert text to camelCase
@@ -12,7 +10,7 @@ function camelCase(str: string) {
     return str.replace(/[-_](\w)/gu, (_, c) => (c ? c.toUpperCase() : ""))
 }
 
-let content = `
+const content = `
 import type { RuleModule } from "../types"
 ${rules
     .map(
@@ -29,13 +27,6 @@ export const rules = [
 `
 
 const filePath = path.resolve(__dirname, "../lib/utils/rules.ts")
-
-if (isWin) {
-    content = content
-        .replace(/\r?\n/gu, "\n")
-        .replace(/\r/gu, "\n")
-        .replace(/\n/gu, "\r\n")
-}
 
 // Update file.
 fs.writeFileSync(filePath, content)
