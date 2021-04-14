feaf1219a8fe826118d2deb2409d4ed895ad4718
Yosuke Ota <otameshiyo23@gmail.com>
2021-04-14 20:19:12 +0900

    Add `regexp/optimal-lookaround-quantifier` rule (#97)
    * Add `regexp/optimal-lookaround-quantifier` rule
    
    * Refactored optimal lookahead quantifier (#101)
    
    Co-authored-by: Michael Schmidt <mitchi5000.ms@googlemail.com>

---
+++ lib/rules/optimal-lookaround-quantifier.ts
@@ -0,0 +1,112 @@
+import type { Expression } from "estree"
+import type { RegExpVisitor } from "regexpp/visitor"
+import type { Alternative, LookaroundAssertion, Quantifier } from "regexpp/ast"
+import { createRule, defineRegexpVisitor, getRegexpLocation } from "../utils"
+
+/**
+ * Extract invalid quantifiers for lookarounds
+ */
+function* extractInvalidQuantifiers(
+    alternatives: Alternative[],
+    kind: LookaroundAssertion["kind"],
+): IterableIterator<Quantifier> {
+    for (const { elements } of alternatives) {
+        if (elements.length > 0) {
+            const lastIndex = kind === "lookahead" ? elements.length - 1 : 0
+            const last = elements[lastIndex]
+            switch (last.type) {
+                case "Quantifier":
+                    if (last.min !== last.max) {
+                        // TODO: last might contain a capturing group in which cause, we can't change the quantifier
+                        yield last
+                    }
+                    break
+
+                case "Group":
+                    yield* extractInvalidQuantifiers(last.alternatives, kind)
+                    break
+
+                // we ignore capturing groups on purpose.
+                // Example: /(?=(a*))\w+\1/ (no ideal but it illustrates the point)
+
+                default:
+                    break
+            }
+        }
+    }
+}
+
+const END_START_PHRASE = {
+    lookahead: "end",
+    lookbehind: "start",
+}
+
+export default createRule("optimal-lookaround-quantifier", {
+    meta: {
+        docs: {
+            description:
+                "disallow the alternatives of lookarounds that end with a non-constant quantifier",
+            // TODO Switch to recommended in the major version.
+            // recommended: true,
+            recommended: false,
+            default: "warn",
+        },
+        schema: [],
+        messages: {
+            remove:
+                "The quantified expression {{expr}} at the {{endOrStart}} of the expression tree should only be matched a constant number of times. The expression can be removed without affecting the lookaround.",
+            replacedWith:
+                "The quantified expression {{expr}} at the {{endOrStart}} of the expression tree should only be matched a constant number of times. The expression can be replaced with {{replacer}} without affecting the lookaround.",
+        },
+        type: "problem",
+    },
+    create(context) {
+        const sourceCode = context.getSourceCode()
+
+        /**
+         * Create visitor
+         * @param node
+         */
+        function createVisitor(node: Expression): RegExpVisitor.Handlers {
+            return {
+                onAssertionEnter(aNode) {
+                    if (
+                        aNode.kind === "lookahead" ||
+                        aNode.kind === "lookbehind"
+                    ) {
+                        const endOrStart = END_START_PHRASE[aNode.kind]
+                        const quantifiers = extractInvalidQuantifiers(
+                            aNode.alternatives,
+                            aNode.kind,
+                        )
+
+                        for (const q of quantifiers) {
+                            const replacer =
+                                q.min === 0
+                                    ? ""
+                                    : q.min === 1
+                                    ? `${q.element.raw} (no quantifier)`
+                                    : `${q.element.raw}{${q.min}}`
+
+                            context.report({
+                                node,
+                                loc: getRegexpLocation(sourceCode, node, q),
+                                messageId:
+                                    q.min === 0 ? "remove" : "replacedWith",
+                                data: {
+                                    expr: q.raw,
+                                    endOrStart,
+                                    replacer,
+                                },
+                            })
+                        }
+                    }
+                },
+            }
+        }
+
+        return defineRegexpVisitor(context, {
+            createVisitor,
+        })
+    },
+})
