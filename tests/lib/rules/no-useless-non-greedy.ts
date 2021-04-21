92af1d6317d7a3743794a9395d96170eca3f049c
yosuke ota <otameshiyo23@gmail.com>
2021-04-21 18:02:49 +0900

    Remove dupe test cases

--- tests/lib/rules/no-useless-non-greedy.ts
+++ tests/lib/rules/no-useless-non-greedy.ts
@@ -9,72 +9,12 @@ const tester = new RuleTester({
 })
 
 tester.run("no-useless-non-greedy", rule as any, {
-    valid: [
-        `/a*?/`,
-        `/a+?/`,
-        `/a{4,}?/`,
-        `/a{2,4}?/`,
-        `/a{2,2}/`,
-        `/a{3}/`,
-        `/a+?b*/`,
-        `/[\\s\\S]+?bar/`,
-        `/a??a?/`,
-    ],
+    valid: [`/a*?/`],
     invalid: [
         {
             code: `/a{1}?/`,
             output: `/a{1}/`,
-            errors: [
-                {
-                    messageId: "constant",
-                    line: 1,
-                    column: 6,
-                    endLine: 1,
-                    endColumn: 7,
-                },
-            ],
-        },
-        {
-            code: `/a{4}?/`,
-            output: `/a{4}/`,
-            errors: [
-                {
-                    messageId: "constant",
-                    line: 1,
-                    column: 6,
-                    endLine: 1,
-                    endColumn: 7,
-                },
-            ],
-        },
-        {
-            code: `/a{2,2}?/`,
-            output: `/a{2,2}/`,
-            errors: [{ messageId: "constant" }],
-        },
-        {
-            code: String.raw`const s = "\\d{1}?"
-            new RegExp(s)`,
-            output: String.raw`const s = "\\d{1}"
-            new RegExp(s)`,
-            errors: [{ messageId: "constant" }],
-        },
-        {
-            code: String.raw`const s = "\\d"+"{1}?"
-            new RegExp(s)`,
-            output: null,
-            errors: [{ messageId: "constant" }],
-        },
-
-        {
-            code: `/a+?b+/`,
-            output: `/a+b+/`,
-            errors: [{ messageId: "possessive" }],
-        },
-        {
-            code: `/(?:a|cd)+?(?:b+|zzz)/`,
-            output: `/(?:a|cd)+(?:b+|zzz)/`,
-            errors: [{ messageId: "possessive" }],
+            errors: 1,
         },
     ],
 })
