1718c81685cdb5d167ceb301a39123e8931e8aef
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-08-14 14:22:32 +0200

    Add more playground examples

--- docs/.vuepress/components/demo/demo-code.js
+++ docs/.vuepress/components/demo/demo-code.js
@@ -1,3 +1,17 @@
-/eslint[-plugin-regexp]/u
+/eslint-plugin[-regexp]/u
 
-const re = /[a-zA-Z_0-9][A-Z_\da-z]*\e{1,}/
+// Optimize and Simplify
+
+var re = /[0-9][^\s]/iu;
+var re = /[\w\p{ASCII}]/u;
+var re = /^\w[_A-Z\d]*\e{1,}/i;
+
+var re = /(?:\w|\d){1,}/;
+var re = /(?<!\w)a+(?=$)/mi;
+var re = /[\s\S]#[\0-\uFFFF]/yi;
+var re = /\d*\w(?:[a-z_]|\d+)*/im;
+
+// Detect problems
+
+var re = /\1(a)/;
+var re = RegExp('[a-z]+' + '|Foo', 'i');
