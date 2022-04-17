e27179ae9b6529852d9a258f9f4b73d57582fb7f
Michael Schmidt <mitchi5000.ms@googlemail.com>
2022-04-17 13:08:58 +0200

    Adjusted no-dupe-disjunctions tests

--- tests/lib/rules/no-dupe-disjunctions.ts
+++ tests/lib/rules/no-dupe-disjunctions.ts
@@ -17,13 +17,9 @@ tester.run("no-dupe-disjunctions", rule as any, {
         `/(?:js|json)$/`,
         `/(?:js|json)abc/`,
         `/(?:js|json)?abc/`,
-        `/(?:yml|ya?ml)$/`,
-        `/(?:yml|ya?ml)/`,
         `/<("[^"]*"|'[^']*'|[^'">])*>/g`,
         `/c+|[a-f]/`,
         String.raw`/b+(?:\w+|[+-]?\d+)/`,
-        String.raw`/A+_|A*_/`,
-        String.raw`/(?:A+|A*)_/`,
         String.raw`/\d*\.\d+_|\d+\.\d*_/`,
         String.raw`/\d*\.\d+|\d+\.\d*/`,
         String.raw`/(?:\d*\.\d+|\d+\.\d*)_/`,
@@ -313,7 +309,13 @@ tester.run("no-dupe-disjunctions", rule as any, {
             ],
         },
         {
-            code: `/(?:ya?ml|yml)/`,
+            code: `/(?:yml|ya?ml)$/`,
+            errors: [
+                "Unexpected useless alternative. This alternative is a strict subset of 'ya?ml' and can be removed.",
+            ],
+        },
+        {
+            code: `/(?:yml|ya?ml)/`,
             errors: [
                 "Unexpected useless alternative. This alternative is a strict subset of 'ya?ml' and can be removed.",
             ],
@@ -498,6 +500,18 @@ tester.run("no-dupe-disjunctions", rule as any, {
                     column: 5,
                     endColumn: 7,
                 },
+            ],
+        },
+        {
+            code: String.raw`/A+_|A*_/`,
+            errors: [
+                "Unexpected useless alternative. This alternative is a strict subset of 'A*_' and can be removed.",
+            ],
+        },
+        {
+            code: String.raw`/(?:A+|A*)_/`,
+            errors: [
+                "Unexpected useless alternative. This alternative is a strict subset of 'A*' and can be removed.",
             ],
         },
         {
