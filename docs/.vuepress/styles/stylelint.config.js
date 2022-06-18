7254a8131edebaff42eb809cfeea7af6e8a8f43d
yosuke ota <otameshiyo23@gmail.com>
2022-06-18 10:37:03 +0900

    chore: use stylelint-stylus

--- docs/.vuepress/styles/stylelint.config.js
+++ docs/.vuepress/styles/stylelint.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    extends: ["stylelint-config-standard", "stylelint-plugin-stylus/standard"],
+    extends: ["stylelint-config-standard", "stylelint-stylus/standard"],
     rules: {
         "no-descending-specificity": null,
+        "selector-class-pattern": null,
+        "value-keyword-case": null,
     },
 }
