f803025402f2bce6ac938d188bec35fb5b9c7a80
ota-meshi <otameshiyo23@gmail.com>
2023-09-14 09:06:54 +0900

    add test for string

--- tests/lib/rules/prefer-d.ts
+++ tests/lib/rules/prefer-d.ts
@@ -35,6 +35,7 @@ tester.run("prefer-d", rule as any, {
         },
         String.raw`/[\q{0|1|2|3|4|5|6|7|8}]/v`,
         String.raw`/[\q{0|1|2|3|4|5|6|7|8|9|a}]/v`,
+        String.raw`/[\q{0|1|2|3|4|5|6|7|8|9|abc}]/v`,
     ],
     invalid: [
         {
