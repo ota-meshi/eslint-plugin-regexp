2123a4f9a365939d7b6bc217cd01f8655ccceedd
kazuya kawaguchi <kawakazu80@gmail.com>
2026-01-21 15:26:20 +0900

    fix: simplfy tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from "tsdown"
 
 export default defineConfig({
-    clean: true,
     tsconfig: "tsconfig.build.json",
     dts: true,
-    outDir: "dist",
     entry: ["lib/index.ts"],
-    format: ["esm"],
-    unbundle: true, // transpile only like `tsc`
-    fixedExtension: false,
+    fixedExtension: false
 })
