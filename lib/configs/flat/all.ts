e7682fe3ed8a5a2e83d21aba2fa9e6f84eeabdaf
kazuya kawaguchi <kawakazu80@gmail.com>
2026-01-21 15:25:57 +0900

    fix: use plugin simply, not use node:module

--- lib/configs/flat/all.ts
+++ lib/configs/flat/all.ts
@@ -1,11 +1,9 @@
-import module from "node:module"
+import plugin from "../../index.ts"
 export { rules } from "../rules/all.ts"
-
-const require = module.createRequire(import.meta.url)
 
 export const plugins = {
     // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types -- ignore for dynamically plugin import
     get regexp() {
-        return require("../../index.js").default
+        return plugin
     },
 }
