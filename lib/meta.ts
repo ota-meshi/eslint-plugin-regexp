67ea62afa5f0b970d71dcdd4e9d284caf7f59872
yosuke ota <otameshiyo23@gmail.com>
2026-01-22 18:01:15 +0900

    chore: remove unnecessary comment

--- lib/meta.ts
+++ lib/meta.ts
@@ -1,5 +1,3 @@
-// note - cannot migrate this to an import statement because it will make TSC copy the package.json to the dist folder
-
 import { default as pkg } from "../package.json" with { type: "json" }
 
 export const name = pkg.name
